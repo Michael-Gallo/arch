9f7f4c64f798935771a2bbb1dfa2ad300a68862f
Michael A Gallo <michaelgallo@protonmail.com>
2021-11-26 12:14:04 -0500

    Reworked install script

--- install.sh
+++ install.sh
@@ -1,20 +1,70 @@
-#!/bin/bash
+#!/bin/sh
 
-#install git and yay so aur packages can be installed
+#install paru
+echo "Installing Paru"
 sudo pacman -S git
 pushd $HOME
-git clone https://aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si
 # get back to this folder
 cd $HOME
-sudo rm -r ./yay
+sudo rm -r ./paru
 popd
+echo "Setting github"
+# Set github and gitlab to always use ssh
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
+git config --global --add url."git@gitlab.com:".insteadOf "https://github.com/"
+# set git user and email
+echo "Enter your email"
+read email
+echo "Enter your name"
+read name
+git config --global user.email $email
+git config --global user.name $name
+
+# Prompt to add ssh to gitlab/hub
+echo "Please set up an SSH key (ssh-keygen) and add id_rsa.pub to github/lab"
+read X
+
+
 # install packages from packages.txt
-sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort packages.txt))
-# install AUR packages, need keyserver fix for libxft to work
-gpg --keyserver pool.sks-keyservers.net --recv-keys  4A193C06D35E7C670FA4EF0BA2FB9E081F2D130E
-yay -S - < foreign_packages.txt
-# install exa
-cargo install exa
-# install flatpaks
+echo "Installing necessary packages"
+paru -S --needed $(comm -12 <(pacman -Slq | sort) <(sort packages.txt))
+# Pull dotfiles
+pushd $HOME
+echo "Installing dotfiles"
+yadm clone git@github.com:Michael-Gallo/Dotfiles.git
+
+# Set the shell to zsh
+echo "Changing shell to zsh"
+chsh --shell /bin/zsh $USER
+# Install antibody extensions
+pushd $HOME/.config/zsh
+$HOME/.config/zsh/install_zsh.sh
+popd
+# Config currently needs this file
+touch $HOME/.config/bash/private_env
+# Install dwm
+echo "Installing DWM"
+pushd $HOME
+git clone git@github.com:Michael-Gallo/dwm.git
+cd dwm
+sudo make clean install
+cd blocks
+sudo make clean install
+popd
+# Install dmenu
+echo "Installing Dmenu"
+pushd $HOME
+git clone git@github.com:Michael-Gallo/dmenu.git
+cd dmenu
+sudo make clean install
+popd
+# Install Luke Smith's built of ST
+echo "Installing st (Luke Smith's build)"
+git clone git@github.com:LukeSmithxyz/st.git
+cd st
+sudo make install
+cd ..
+rm -r ./st
