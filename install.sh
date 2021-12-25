06384626388ed1b00018486840256af7a8a85bff
Michael A Gallo <michaelgallo@protonmail.com>
2021-12-25 13:43:12 -0500

    Fixed multiple errors

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-#install paru
+
+# #install paru
 echo "Installing Paru"
-sudo pacman -S git
+sudo pacman --needed -S git base-devel
 pushd $HOME
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 # get back to this folder
-cd $HOME
-sudo rm -r ./paru
 popd
+# Get rid of the paru install directory
+sudo rm -r ~/paru
+
+
 echo "Setting github"
-# Set github and gitlab to always use ssh
 # set git user and email
 echo "Enter your email"
 read email
 echo "Enter your name"
 read name
-git config --global user.email $email
-git config --global user.name $name
+git config --global user.email "$email"
+git config --global user.name "$name"
 
 # Prompt to add ssh to gitlab/hub
 echo "Please set up an SSH key (ssh-keygen) and add id_rsa.pub to github/lab"
@@ -29,24 +31,27 @@ read X
 # install packages from packages.txt
 echo "Installing necessary packages"
 paru -S --needed $(comm -12 <(pacman -Slq | sort) <(sort packages.txt))
-# Pull dotfiles
+# # Pull dotfiles
 pushd $HOME
 echo "Installing dotfiles"
-yadm clone git@github.com:Michael-Gallo/Dotfiles.git
-
+yadm clone git@gitlab.com:michaelagallo95/dotfiles.git
+yadm remote set-url --add --push origin git@github.com:Michael-Gallo/dotfiles.git
 # Set the shell to zsh
 echo "Changing shell to zsh"
-chsh --shell /bin/zsh $USER
+sudo chsh -s /bin/zsh $USER
 # Install antibody extensions
 pushd $HOME/.config/zsh
+antibody bundle < ./zsh_plugins.txt > zsh_plugins.sh
 $HOME/.config/zsh/install_zsh.sh
 popd
 # Config currently needs this file
+mkdir -p $HOME/.config/bash
 touch $HOME/.config/bash/private_env
 # Install dwm
 echo "Installing DWM"
 pushd $HOME
-git clone git@github.com:Michael-Gallo/dwm.git
+git clone git@gitlab.com:michaelagallo95/dwm.git
+git remote set-url --add --push origin git@github.com:Michael-Gallo/dwm.git
 cd dwm
 sudo make clean install
 cd blocks
@@ -57,6 +62,7 @@ sudo cp dwm.desktop /usr/share/xsessions
 echo "Installing dwl"
 pushd $HOME
 git clone git@gitlab.com:michaelagallo95/dwl.git
+git remote set-url --add --push origin git@github.com:Michael-Gallo/dwl.git
 cd dwl
 sudo make clean install
 popd
@@ -64,13 +70,15 @@ sudo cp dwl.desktop /usr/share/wayland-sessions
 # Install dmenu
 echo "Installing Dmenu"
 pushd $HOME
-git clone git@github.com:Michael-Gallo/dmenu.git
+git clone git@gitlab.com:michaelagallo95/dmenu.git
+git remote set-url --add --push origin git@github.com:Michael-Gallo/dmenu.git
 cd dmenu
 sudo make clean install
 popd
 # Install Luke Smith's built of ST
+pushd $HOME
 echo "Installing st (Luke Smith's build)"
-git clone git@github.com:LukeSmithxyz/st.git
+git clone https://github.com/LukeSmithxyz/st.git
 cd st
 sudo make install
-cd ..
+popd
